94926d8d05c745794c4856bccf8a9d3f50d452c6
Anton Alekseev <toxa23@gmail.com>
2015-05-31 01:09:33 +0300

    Missing files for shadows

---
+++ Assets/Scripts/EnableShadows.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableShadows : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().castShadows = true;
+	}
+}
